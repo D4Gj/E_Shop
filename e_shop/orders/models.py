9dd4fb956772ba3fbf02a9529a20e4ef3b51bbfa
D4Gj <inzadimonax@gmail.com>
2021-11-08 01:44:40 +0400

    Initial commit

---
+++ e_shop/orders/models.py
@@ -0,0 +1,8 @@
+# from django.db import models
+# from .models import Product
+#
+# class Order(models.Model):
+#     first_name = models.CharField(max_length=50)
+#     last_name = models.CharField(max_length=50)
+#     email = models.EmailField()
+#     #address
